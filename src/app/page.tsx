15c44371af76635a88b76ab492aafa7368c72837
Oktamov Shokhjahon <oktamovshokhjahon@gmail.com>
2025-04-24 20:10:09 +0500

    import fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import GameCarousel from "./components/carousel";
+// import GameCarousel from "./components/carousel";
 import GameMarketplace from "./components/game-cards/game-marketplace";
 
 function page() {
